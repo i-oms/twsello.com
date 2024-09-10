1d72fae2e5d0d0c6ce0e56d85f437bc44b8f9b9a
sharafas-om <sharafasom@gmail.com>
2024-09-10 19:33:23 +0530

    updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548e1","builds":[{"compileTarget":"dart2js","renderer":"skwasm","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548e1","builds":[{"compileTarget":"dart2js","renderer":"auto","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2245439234"
+    serviceWorkerVersion: "844181179"
   }
 });
