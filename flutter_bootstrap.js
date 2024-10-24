fb9346f440c35c6c24fa63ed038bbe3921ece266
sharafas-om <sharafasom@gmail.com>
2024-10-24 14:46:12 +0530

    updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1425714567"
+    serviceWorkerVersion: "2156007413"
   }
 });
